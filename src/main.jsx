f6f6c9c3c533287033f97bedc9b2dd945215882e
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-07-21 13:20:55 -0700

    Forgot to add a BrowserRouter in the main jsx file oops

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './styles/index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./styles/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Selects the DOM element with an ID of root and renders what is inside render to root container.
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
