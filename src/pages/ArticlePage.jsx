cc15319a71cd2145e43d91040f4e8b78be0eb346
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-07-21 16:38:27 -0700

    added comments to ArticlePage jsx file

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 
+// useLocation accesses the current url location. Basically allows access to the article object that was passed as state when navigating this route. Also defaults to an empty object to avoid errors.
 function ArticlePage() {
   const location = useLocation();
   const { article } = location.state || {};
@@ -11,6 +12,8 @@ function ArticlePage() {
         <>
           <h1>{article.title}</h1>
           <p>{article.content}</p>
+
+          // rel=noopener noreferrer is good security practice apparently to use to help prevent potential security threats. When the new page is opened in another tab then this new page will not manipulate the original page.
           <a href={article.url} target="_blank" rel="noopener noreferrer">
             Read More Here
           </a>
