4f61c48a061483afbe748cb1beebbaf37d75e41e
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-07-21 16:01:44 -0700

    added comments to NewsCard jsx

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -3,16 +3,23 @@ import { useNavigate } from "react-router-dom";
 import "../styles/NewsCard.css";
 import placeholder from '../assets/placeholder.jpg';
 
+// An article object represents an individual news article.
+// Index of the article in the list.
 function NewsCard({ article, index }) {
+
+  // Allows you to navigate to different routes.
   const navigate = useNavigate();
 
+  // Called only when a news card is clicked. The id is dynamically placed in to the route using a template literal. Then passes the article object with the navigation.
   const handleClick = () => {
     navigate(`/article/${index}`, { state: { article } });
   };
 
   return (
+    // Adding CSS and making card clickable
     <div className="newsCard" onClick={handleClick}>
       <img
+      // Use article image, if no image is provided by article then use placeholder image downloaded from pexels. I guess i could have used my own photo. Next time.
         src={article.urlToImage || placeholder}
         alt={article.title}
         className="newsCardImage"
